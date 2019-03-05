1bc8ae4222b672bdd6930ea50c725ec4a8b9b963
Kate <kef12@comcast.net>
2019-03-05 13:10:03 -0700

    MVC and routes

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Baby Can You Chive My Car Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sympathy for the Deviled Egg Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bohemian Radishy Burger');
+
+INSERT INTO burgers (devoured) VALUES ("1");
+INSERT INTO burgers (devoured) VALUES ("0");
+INSERT INTO burgers (devoured) VALUES ("1");
