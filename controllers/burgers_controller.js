b80bc2a7e2635297502ecae8add251b484415bc4
Kate <kef12@comcast.net>
2019-03-07 14:26:51 -0700

    fixed routes, added JawsDB and main

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,46 +4,40 @@ var burger = require("../models/burger.js");
 
 // Set Routes
 
-//Index
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+// Index Redirect
+router.get('/', function (req, res) 
+{
+  res.redirect('/index');
+});
+
+// Index Page 
+router.get('/index', function (req, res) 
+{
+  burger.selectAll(function(data) 
+  {
+    var hbsObject = { burgers: data };
+    //console.log(hbsObject);
+    res.render('index', hbsObject);
   });
 });
 
-//New Burger
-router.post("/burger/create", function(req, res) {
-  burger.create(["name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+// Create a New Burger
+router.post('/burger/create', function (req, res) 
+{
+  burger.insertOne(req.body.burger_name, function() 
+  {
+    res.redirect('/index');
   });
 });
 
-
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      devoured: req.body.devoured
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-
-    }
-  );
+// Devour a Burger
+router.post('/burger/eat/:id', function (req, res) 
+{
+  burger.updateOne(req.params.id, function() 
+  {
+    res.redirect('/index');
+  });
 });
 
-// Export routes for server.js to use.
+// Export routes
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,32 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
+    selectAll: function(callback)
+    {
+      orm.selectAll(function(res)
+      {
+        callback(res);
+      });
+    },
+  
+    insertOne: function(burger_name, callback)
+    {
+      orm.insertOne(burger_name, function(res)
+      {
+        callback(res);
+      });
+    },
+  
+    updateOne: function(burger_id, callback)
+    {
+      orm.updateOne(burger_id, function(res)
+      {
+        callback(res);
+      });
+    }
+  
+  };
+  
 
 // Exports the database functions for the controller.
 module.exports = burger;
