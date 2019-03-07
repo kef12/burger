940045226cf0de01f2533853e680bf6efe1b40b5
Kate <kef12@comcast.net>
2019-03-07 13:04:26 -0700

    fixed mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,6 +2,6 @@ INSERT INTO burgers (burger_name) VALUES ('Baby Can You Chive My Car Burger');
 INSERT INTO burgers (burger_name) VALUES ('Sympathy for the Deviled Egg Burger');
 INSERT INTO burgers (burger_name) VALUES ('Bohemian Radishy Burger');
 
-INSERT INTO burgers (devoured) VALUES ("1");
-INSERT INTO burgers (devoured) VALUES ("0");
-INSERT INTO burgers (devoured) VALUES ("1");
+INSERT INTO burgers (devoured) VALUES (false);
+INSERT INTO burgers (devoured) VALUES (false);
+INSERT INTO burgers (devoured) VALUES (false);
